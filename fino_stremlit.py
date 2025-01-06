1453991a0ed5cbaf062d4d0656ba1be82c654db8
Luisa Beccar Varela <159951478+LuisaBeccar@users.noreply.github.com>
2025-01-06 12:35:56 -0300

    Update fino_stremlit.py

--- fino_stremlit.py
+++ fino_stremlit.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1J9Opw_YkwLejb5Rurwepi90KNUztl1bF
 """
 
-pip install streamlit pandas matplotlib spacy scikit-learn
 
 import streamlit as st
 import pandas as pd
